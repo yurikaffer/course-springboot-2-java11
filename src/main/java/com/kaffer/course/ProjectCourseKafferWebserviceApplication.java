ad973f391d65a01723f89f8b027168789a9e9882
Yuri Kaffer <yurikaffer@gmail.com>
2021-04-24 16:01:58 -0300

    Project created

---
+++ src/main/java/com/kaffer/course/ProjectCourseKafferWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.kaffer.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCourseKafferWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCourseKafferWebserviceApplication.class, args);
+	}
+
+}
