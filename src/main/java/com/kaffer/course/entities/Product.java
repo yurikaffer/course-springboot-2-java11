52c8f77aa48ba675e762415fd864142829860a28
Yuri Kaffer <yurikaffer@gmail.com>
2021-04-25 23:20:24 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/kaffer/course/entities/Product.java
+++ src/main/java/com/kaffer/course/entities/Product.java
@@ -1,6 +1,5 @@
 package com.kaffer.course.entities;
 
-
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
@@ -9,13 +8,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
-	
+public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	@Id
@@ -25,14 +25,16 @@ public class Product implements Serializable{
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	@Transient
-	private Set<Category> categories= new HashSet<>();
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+
+	private Set<Category> categories = new HashSet<>();
 
 	public Product() {
-		
+
 	}
-	
+
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
@@ -116,6 +118,5 @@ public class Product implements Serializable{
 		return "Product [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price
 				+ ", imgUrl=" + imgUrl + ", categories=" + categories + "]";
 	}
-	
-	
+
 }
