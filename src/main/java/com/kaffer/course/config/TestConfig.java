f1098390a4a80c71d1ba46eec11f16c2b9bea3d1
Yuri Kaffer <yurikaffer@gmail.com>
2021-04-30 20:38:48 -0300

    Payment, one-to-one association

--- src/main/java/com/kaffer/course/config/TestConfig.java
+++ src/main/java/com/kaffer/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.kaffer.course.entities.Category;
 import com.kaffer.course.entities.Order;
 import com.kaffer.course.entities.OrderItem;
+import com.kaffer.course.entities.Payment;
 import com.kaffer.course.entities.Product;
 import com.kaffer.course.entities.User;
 import com.kaffer.course.entities.enums.OrderStatus;
@@ -81,5 +82,14 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
+		
+		
+		
 	}
 }
