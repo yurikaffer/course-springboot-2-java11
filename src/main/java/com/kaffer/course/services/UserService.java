170be1aae3d07026d3ef17f7383225aaef966190
Yuri Kaffer <yurikaffer@gmail.com>
2021-04-30 21:44:12 -0300

    User insert

--- src/main/java/com/kaffer/course/services/UserService.java
+++ src/main/java/com/kaffer/course/services/UserService.java
@@ -18,14 +18,18 @@ public class UserService {
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		repository.deleteById(id);
 	}
 
 }
